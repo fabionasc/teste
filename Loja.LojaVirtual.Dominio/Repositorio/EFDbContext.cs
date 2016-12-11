b5e4b9bfd2c702f994c40d8e253c7a99b58aab49
Fabio <Fabio@VM-WIN8>
2016-12-11 01:10:16 -0200

    AddView

---
+++ Loja.LojaVirtual.Dominio/Repositorio/EFDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using Loja.LojaVirtual.Dominio.Entidade;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Loja.LojaVirtual.Dominio.Repositorio
+{
+    public class EFDbContext :DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Entity<Produto>().ToTable("Produtos");
+        }
+
+    }
+}
---
+++ Loja.LojaVirtual.Dominio/Repositorio/ProdutosRepositorio.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Loja.LojaVirtual.Dominio.Entidade;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Loja.LojaVirtual.Dominio.Repositorio
+{
+    public class ProdutosRepositorio
+    {
+        private readonly EFDbContext _context = new EFDbContext();
+        public IEnumerable<Produto> Produtos
+        {
+            get { return _context.Produtos; }
+        }
+            
+
+    }
+}
